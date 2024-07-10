17eedd9058a89947c6f98a7abc2d0e5167e247e6
Nithesh kumar <117974041+Nithesh7708@users.noreply.github.com>
2024-07-10 23:58:01 +0530

    completed menubar

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+let menu_btn=document.querySelector('.nav_container .right .menu_icon');
+let close_btn=document.querySelector('.nav_container .right .menu_close');
+menu_btn.addEventListener("click",()=>{
+    document.body.classList.add('scroll')
+    document.getElementById("menubar").classList.add("open")
+})
+close_btn.addEventListener('click',()=>{
+    document.body.classList.remove('scroll')
+    document.getElementById("menubar").classList.remove("open")
+})
